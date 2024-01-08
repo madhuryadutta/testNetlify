85a71011614230eb433a2419d33e1180f86efcf3
madhuryadutta <madhuryadutta@users.noreply.github.com>
2024-01-08 15:58:39 +0000

    :art: Format Python code with psf/black

--- python_List_To_DIctionary_to_JSON_Convert/main.py
+++ python_List_To_DIctionary_to_JSON_Convert/main.py
@@ -1,7 +1,7 @@
-import json 
-    
+import json
+
 f = open("wordlist.txt", "r")
-myString =f.read()
+myString = f.read()
 myList = myString.split()
 # print(len(myList))
 
@@ -10,8 +10,8 @@ res = []
 [res.append(x) for x in myList if x not in res]
 res_dict = {}
 for i in range(0, len(myList)):
-    res_dict[myList[i]] =1
+    res_dict[myList[i]] = 1
 # print(res_dict)
 # Convert and write JSON object to file
-with open("wordlist.json", "w") as outfile: 
+with open("wordlist.json", "w") as outfile:
     json.dump(res_dict, outfile)
