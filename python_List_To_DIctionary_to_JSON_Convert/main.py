98efe142ea971c77980271942944b987c5e8f799
Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>
2024-01-08 15:37:08 +0000

    added more word , added duplicate check (using set)

--- python_List_To_DIctionary_to_JSON_Convert/main.py
+++ python_List_To_DIctionary_to_JSON_Convert/main.py
@@ -3,10 +3,15 @@ import json
 f = open("wordlist.txt", "r")
 myString =f.read()
 myList = myString.split()
+# print(len(myList))
+
+# using list comprehension to remove duplicated from list
+res = []
+[res.append(x) for x in myList if x not in res]
 res_dict = {}
 for i in range(0, len(myList)):
     res_dict[myList[i]] =1
-print(res_dict)
+# print(res_dict)
 # Convert and write JSON object to file
 with open("wordlist.json", "w") as outfile: 
     json.dump(res_dict, outfile)
